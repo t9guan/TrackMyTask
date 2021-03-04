f876221ef108723de1f08a5abd4a62aa6c35a71c
Kazuha Miyoshi <kmiyoshi@ucsd.edu>
2021-03-04 11:55:36 -0800

    FAQ

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var task = require('./routes/task');
 var add = require('./routes/add')
 var login = require('./routes/login');
 var register = require('./routes/register');
+var faq = require('./routes/faq');
 var log = require('./routes/log');
 var edit = require('./routes/edit');
 var help = require('./routes/help');
@@ -57,6 +58,7 @@ app.get('/profile', profile.view);
 app.get('/views/index.handlebars', index.view);
 app.get('/views/login.handlebars', login.view);
 app.get('/views/register.handlebars', register.view);
+app.get('/views/faq.handlebars', faq.view);
 // Example route
 // app.get('/users', user.list);
 
